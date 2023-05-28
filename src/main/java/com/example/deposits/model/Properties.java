8b16f164983a5aba1e346aea01a4c1db7cbf98ef
imgyash <g.yashwanth896@gmail.com>
2023-05-28 12:56:07 -0400

    Swagger Documentation

--- src/main/java/com/example/deposits/model/Properties.java
+++ src/main/java/com/example/deposits/model/Properties.java
@@ -1,5 +1,6 @@
 package com.example.deposits.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,9 +10,27 @@ import java.util.List;
 
 @Getter @Setter
 @AllArgsConstructor
+@Schema(
+        description = "Properties Model"
+)
 public class Properties {
+    @Schema(
+            description = "Message attribute"
+    )
     private String msg;
+
+    @Schema(
+            description = "Build Version"
+    )
     private String buildVersion;
+
+    @Schema(
+            description = "Mail Details"
+    )
     private Map<String, String> mailDetails;
+
+    @Schema(
+            description = "Active Branches"
+    )
     private List<String> activeBranches;
 }
