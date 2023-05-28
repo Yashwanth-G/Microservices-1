1f67e4db9b8d6ffd6150a4b58abccbb0bb8679f6
imgyash <g.yashwanth896@gmail.com>
2023-05-28 12:55:24 -0400

    Swagger Documentation

--- src/main/java/com/example/deposits/model/Customer.java
+++ src/main/java/com/example/deposits/model/Customer.java
@@ -1,9 +1,16 @@
 package com.example.deposits.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
-
+@Schema(
+        description = "Customer Model"
+)
 @Getter @Setter
 public class Customer {
+
+    @Schema(
+            description = "customer ID"
+    )
     private int customerId;
 }
