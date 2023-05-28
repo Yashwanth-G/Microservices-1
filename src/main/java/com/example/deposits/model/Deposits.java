1b7a36d38ee7ad46372747c12c8578c5d48c9be5
imgyash <g.yashwanth896@gmail.com>
2023-05-28 12:55:47 -0400

    Swagger Documentation

--- src/main/java/com/example/deposits/model/Deposits.java
+++ src/main/java/com/example/deposits/model/Deposits.java
@@ -1,5 +1,6 @@
 package com.example.deposits.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,15 +10,30 @@ import java.time.LocalDate;
 
 @Entity
 @Getter @Setter @ToString
+@Schema(
+        description = "Deposits Model"
+)
 public class Deposits {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Schema(
+            description = "Deposit ID"
+    )
     private int depositId;
 
+    @Schema(
+            description = "Customer ID"
+    )
     private int customerId;
 
+    @Schema(
+            description = "Start Date"
+    )
     private LocalDate startDt;
 
+    @Schema(
+            description = "End Date"
+    )
     private LocalDate endDt;
 }
